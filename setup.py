bf089502e39911cd315fa95f4420eb3c62f49fb8
muhmad <muhmad@live.no>
2024-07-28 16:22:27 +0200

    test

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="a7df32de3a60dfdb7a0b",
+    version="0.13.10",
+    packages=find_packages(),
+    install_requires=[
+        "snscrape",
+        "exorde_data",
+        "aiohttp",
+        "python-dotenv",
+        "selenium==4.2.0",
+        "twikit",
+        "pathlib"
+    ],
+    extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
+)
